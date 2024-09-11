a1fdde0f64d76dcabb94ce28aaa73238aadbe039
Alejandro Nadal <alejandro.nadal@synics.ch>
2024-09-11 19:52:50 +0200

    feat: bitwise and and xor

--- src/main.rs
+++ src/main.rs
@@ -175,6 +175,26 @@ impl Chip8 {
                     third_nibble
                 );
             }
+            (0x08, _, _, 2) => {
+                let val1 = self.registers[second_nibble as usize];
+                let val2 = self.registers[third_nibble as usize];
+                self.registers[second_nibble as usize] = val1 & val2;
+                trace!(
+                    "Bitwise and between registers {} {}",
+                    second_nibble,
+                    third_nibble
+                );
+            }
+            (0x08, _, _, 3) => {
+                let val1 = self.registers[second_nibble as usize];
+                let val2 = self.registers[third_nibble as usize];
+                self.registers[second_nibble as usize] = val1 ^ val2;
+                trace!(
+                    "Bitwise XOR between registers {} {}",
+                    second_nibble,
+                    third_nibble
+                );
+            }
             (0x08, _, _, 4) => {
                 let xpos = second_nibble as usize;
                 let ypos = third_nibble as usize;
